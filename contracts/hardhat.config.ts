0b442e6ace4125bb308cbf94d8f20a50d5cde3ab
patty111 <smefyb@gmail.com>
2024-09-02 23:32:00 +0800

    hardhat config settings

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,7 @@
 // Tasks
 import "./tasks";
 import "@nomicfoundation/hardhat-toolbox";
+import { TIMEOUT } from "dns";
 import {config as dotenvConfig} from "dotenv";
 import "fhenix-hardhat-docker";
 import "fhenix-hardhat-plugin";
@@ -10,18 +11,34 @@ import {HardhatUserConfig} from "hardhat/config";
 import {resolve} from "path";
 
 // DOTENV_CONFIG_PATH is used to specify the path to the .env file for example in the CI
-const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+const dotenvConfigPath: string = "./.env";
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
-
 
 const TESTNET_CHAIN_ID = 8008135;
 const TESTNET_RPC_URL = "https://api.helium.fhenix.zone";
-const keys = process.env.KEY;
 
 const testnetConfig = {
     chainId: TESTNET_CHAIN_ID,
     url: TESTNET_RPC_URL,
-    accounts: [keys],
+    TIMEOUT: 200000,
+}
+
+
+// Select either private keys or mnemonic from .env file or environment variables
+const keys = process.env.KEY;
+if (!keys) {
+  let mnemonic = process.env.MNEMONIC;
+  console.log("mnemonic", mnemonic);
+  if (!mnemonic) {
+    throw new Error("No mnemonic or private key provided, please set MNEMONIC or KEY in your .env file");
+  }
+  testnetConfig['accounts'] = {
+    count: 10,
+    mnemonic,
+    path: "m/44'/60'/0'/0",
+  }
+} else {
+  testnetConfig['accounts'] = [keys];
 }
 
 
